13cc800d204b1eabbf686f46f27b8f5404d5905a
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2021-07-30 09:55:15 +0600

    Added saving to program and a lot of comments to code

---
+++ MinecraftClicker/ClickerConfigManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace MinecraftClicker
+{
+    class ClickerConfigManager
+    {
+        public int savedBreakInterval = 0;
+        public int savedBuildInterval = 0;
+        public int savedReplaceInterval = 0;
+
+        public Keys savedQuickBreakKey;
+        public Keys savedQuickBuildKey;
+        public Keys savedQuickReplaceKey;
+
+        public ClickerConfigManager(int breakInterval, int buildInterval, int replaceInterval,
+                           Keys quickBreakKey, Keys quickBulidKey, Keys quickReplaceKey)
+        {
+            savedQuickBreakKey = quickBreakKey;
+            savedQuickBuildKey = quickBulidKey;
+            savedQuickReplaceKey = quickReplaceKey;
+
+            savedBreakInterval = breakInterval;
+            savedBuildInterval = buildInterval;
+            savedReplaceInterval = replaceInterval;
+        }
+    }
+}
--- MinecraftClicker/MainWindow.xaml.cs
+++ MinecraftClicker/MainWindow.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Forms;
 using Utilities;
-using System.Windows.Resources;
-using System.Windows.Media;
-using System.Media;
+using Newtonsoft.Json;
 
 namespace MinecraftClicker
 {
@@ -18,21 +16,23 @@ namespace MinecraftClicker
         bool breakingBlocks = false;
         bool buildingBlocks = false;
 
-        int breakInterval = 500;
-        int buildInterval = 500;
-        int replaceInterval = 100;
+        int breakInterval = 500;   // Default value
+        int buildInterval = 500;   // Default value
+        int replaceInterval = 100; // Default value
 
-        System.Windows.Forms.Keys quickBreakKey = Keys.P;
-        System.Windows.Forms.Keys quickBuildKey = Keys.B;
-        System.Windows.Forms.Keys quickReplaceKey = Keys.F;
+        Keys quickBreakKey = Keys.P;   // Default value
+        Keys quickBuildKey = Keys.B;   // Default value
+        Keys quickReplaceKey = Keys.F; // Default value
 
         Timer breakerTimer;
         Timer builderTimer;
 
         GlobalKeyboardHook gkh = new GlobalKeyboardHook();
+
+        ClickerConfigManager changedClickerConfig; // Used for applying changes
         void gkh_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
-            // Breaking code
+            // Breaking block
             if (e.KeyCode == quickBreakKey)
             {
                 if (!breakingBlocks)
@@ -46,7 +46,7 @@ namespace MinecraftClicker
                     breakingBlocks = !breakingBlocks;
                 }
             }
-            // Building code
+            // Building block
             else if(e.KeyCode == quickBuildKey)
             {
                 if (!buildingBlocks)
@@ -60,15 +60,10 @@ namespace MinecraftClicker
                     buildingBlocks = !buildingBlocks;
                 }
             }
-            // Replacing code
+            // Replacing block
             else if(e.KeyCode == quickReplaceKey)
             {
                 ReplaceBlock();
-            }
-            // Self destruct code
-            else if (e.KeyCode == Keys.X)
-            {
-                Process.GetCurrentProcess().Kill();
             }
         }
 
@@ -92,11 +87,32 @@ namespace MinecraftClicker
         public MainWindow()
         {
             InitializeComponent();
-            gkh.HookedKeys.Add(quickBreakKey);
-            gkh.HookedKeys.Add(quickBuildKey);
-            gkh.HookedKeys.Add(quickReplaceKey);
-            gkh.KeyDown += new System.Windows.Forms.KeyEventHandler(gkh_KeyDown);
 
+            // If file exists -> changing default values
+            // In worst case clicker will simply work with default values [No exceptions =)]
+            if (File.Exists(Environment.CurrentDirectory + "\\saved_config.json"))
+            {
+                StreamReader sr = new StreamReader(Environment.CurrentDirectory + "\\saved_config.json");
+                string savedJsonConfig = sr.ReadToEnd();
+                sr.Close();
+
+                ClickerConfigManager savedClickerConfig = JsonConvert.DeserializeObject<ClickerConfigManager>(savedJsonConfig);
+
+                quickBreakKey = savedClickerConfig.savedQuickBreakKey;      //
+                quickBuildKey = savedClickerConfig.savedQuickBuildKey;       //
+                quickReplaceKey = savedClickerConfig.savedQuickReplaceKey;    //
+                //==============================================================> Changing default values
+                breakInterval = savedClickerConfig.savedBreakInterval;       //
+                buildInterval = savedClickerConfig.savedBuildInterval;      //
+                replaceInterval = savedClickerConfig.savedReplaceInterval; //
+            }
+
+            gkh.HookedKeys.Add(quickBreakKey);   // Now we're looking after this button
+            gkh.HookedKeys.Add(quickBuildKey);   // And this button
+            gkh.HookedKeys.Add(quickReplaceKey); // And this one too
+            gkh.KeyDown += new System.Windows.Forms.KeyEventHandler(gkh_KeyDown); // And will use this method if any of them will be pressed
+
+            //=========================Setting timers up===========================
             breakerTimer = new Timer();
             breakerTimer.Interval = breakInterval;
             breakerTimer.Tick += new EventHandler(BreakBlocks);
@@ -104,38 +120,47 @@ namespace MinecraftClicker
             builderTimer = new Timer();
             builderTimer.Interval = buildInterval;
             builderTimer.Tick += new EventHandler(BuildBlocks);
+            //=====================================================================
+
+            //==========================Applying values============================
+            quickBreakTextBox.Text = Convert.ToString(quickBreakKey);
+            quickBuildTextBox.Text = Convert.ToString(quickBuildKey);
+            quickReplaceTextBox.Text = Convert.ToString(quickReplaceKey);
+
+            quickBreakIntervalTextBox.Text = Convert.ToString(breakInterval);
+            quickBuildIntervalTextBox.Text = Convert.ToString(buildInterval);
+            quickReplaceIntervalTextBox.Text = Convert.ToString(replaceInterval);
+            //=====================================================================
         }
 
+        // If you click on any textbox with key in it and press any button, it will automatically apply pressed key
         private void quickBreakTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             quickBreakTextBox.Text = Convert.ToString(e.Key);
-            Keys formsKey = quickBreakKey;                        // Временная переменная
-            formsKey = (Keys)KeyInterop.VirtualKeyFromKey(e.Key); // Получает нажатую кнопку
-            gkh.HookedKeys.Remove(quickBreakKey);                 // Удаляется старое значение для хука
-            quickBreakKey = formsKey;                             // На действие назначается новая кнопка
-            gkh.HookedKeys.Add(quickBreakKey);                    // Хук отслеживает эту новую кнопку
+            Keys formsKey = (Keys)KeyInterop.VirtualKeyFromKey(e.Key); // formsKey gets a new value (new button)
+            gkh.HookedKeys.Remove(quickBreakKey);                      // Then we delete previous value from our global hook (we won't look after this button)
+            quickBreakKey = formsKey;                                  // Now a new button will do this
+            gkh.HookedKeys.Add(quickBreakKey);                         // And we're looking after this button
             Keyboard.ClearFocus();
         }
 
         private void quickBuildTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             quickBuildTextBox.Text = Convert.ToString(e.Key);
-            Keys formsKey = quickBuildKey;                        // Временная переменная
-            formsKey = (Keys)KeyInterop.VirtualKeyFromKey(e.Key); // Получает нажатую кнопку
-            gkh.HookedKeys.Remove(quickBuildKey);                 // Удаляется старое значение для хука
-            quickBuildKey = formsKey;                             // На действие назначается новая кнопка
-            gkh.HookedKeys.Add(quickBuildKey);                    // Хук отслеживает эту новую кнопку
+            Keys formsKey = (Keys)KeyInterop.VirtualKeyFromKey(e.Key); // formsKey gets a new value (new button)
+            gkh.HookedKeys.Remove(quickBuildKey);                      // Then we delete previous value from our global hook (we won't look after this button)
+            quickBuildKey = formsKey;                                  // Now a new button will do this
+            gkh.HookedKeys.Add(quickBuildKey);                         // And we're looking after this button
             Keyboard.ClearFocus();
         }
 
         private void replaceTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             quickReplaceTextBox.Text = Convert.ToString(e.Key);
-            Keys formsKey = quickReplaceKey;                        // Временная переменная
-            formsKey = (Keys)KeyInterop.VirtualKeyFromKey(e.Key); // Получает нажатую кнопку
-            gkh.HookedKeys.Remove(quickReplaceKey);                 // Удаляется старое значение для хука
-            quickReplaceKey = formsKey;                             // На действие назначается новая кнопка
-            gkh.HookedKeys.Add(quickReplaceKey);                    // Хук отслеживает эту новую кнопку
+            Keys formsKey = (Keys)KeyInterop.VirtualKeyFromKey(e.Key); // formsKey gets a new value (new button)
+            gkh.HookedKeys.Remove(quickReplaceKey);                    // Then we delete previous value from our global hook (we won't look after this button)
+            quickReplaceKey = formsKey;                                // Now a new button will do this
+            gkh.HookedKeys.Add(quickReplaceKey);                       // And we're looking after this button
             Keyboard.ClearFocus();
         }
 
@@ -143,16 +168,19 @@ namespace MinecraftClicker
         {
             if (clickerActivated)
             {
-                gkh.UnHook();
+                gkh.UnHook(); // Now we're not looking after buttons
 
+                // If user's clicker was working -> it will stop it 
                 breakerTimer.Stop();
                 builderTimer.Stop();
+                //===================================================
 
                 clickerActivated = false;
 
                 disableClickerButton.Content = "Activate";
                 Title = "MineClicker (Deactivated)";
 
+                //============Now user can change clicker settings====
                 quickBreakTextBox.IsEnabled = true;
                 quickBreakIntervalTextBox.IsEnabled = true;
 
@@ -161,11 +189,13 @@ namespace MinecraftClicker
 
                 quickReplaceTextBox.IsEnabled = true;
                 quickReplaceIntervalTextBox.IsEnabled = true;
+                //====================================================
             }
             else
             {
-                gkh.Hook();
+                gkh.Hook(); // We're looking after buttons again
 
+                // Checking if user input is OK
                 try
                 {
                     breakInterval = Convert.ToInt32(quickBreakIntervalTextBox.Text);
@@ -173,20 +203,19 @@ namespace MinecraftClicker
                     replaceInterval = Convert.ToInt32(quickReplaceIntervalTextBox.Text);
                     if ((breakInterval <= 0 || breakInterval > 99999) || 
                         (buildInterval <= 0 || buildInterval > 99999) ||
-                        (replaceInterval <= 0 || replaceInterval > 99999))
+                        (replaceInterval <= 0 || replaceInterval > 99999)) // Same here
                     {
                         System.Windows.MessageBox.Show($"Interval value is TOO LOW or TOO HIGHT!", "Value error");
                     }
-                    else 
+                    else // If everything is alright
                     {
-                        clickerActivated = true;
-
-                        breakerTimer.Interval = breakInterval;
-                        builderTimer.Interval = buildInterval;
+                        breakerTimer.Interval = breakInterval; // Applying new value
+                        builderTimer.Interval = buildInterval; // Applying new value
 
                         disableClickerButton.Content = "Deactivate";
                         Title = "MineClicker (Activated)";
 
+                        //============Now user can't change clicker settings====
                         quickBreakTextBox.IsEnabled = false;
                         quickBreakIntervalTextBox.IsEnabled = false;
 
@@ -195,6 +224,19 @@ namespace MinecraftClicker
 
                         quickReplaceTextBox.IsEnabled = false;
                         quickReplaceIntervalTextBox.IsEnabled = false;
+                        //======================================================
+
+                        // When we activate clicker again that means we changed some settings 
+                        // (Sometimes not actually, but that's not the point)
+                        // Settings were changed -> we have to save new info
+                        changedClickerConfig = new ClickerConfigManager(breakInterval, buildInterval, replaceInterval,
+                                                                        quickBreakKey, quickBuildKey, quickReplaceKey);
+                        string savedConfig = JsonConvert.SerializeObject(changedClickerConfig);
+                        StreamWriter sw = new StreamWriter(Environment.CurrentDirectory + "\\saved_config.json");
+                        sw.Write(savedConfig);
+                        sw.Close();
+
+                        clickerActivated = true;
                     }
                 }
                 catch (FormatException)
